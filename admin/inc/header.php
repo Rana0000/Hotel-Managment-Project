10650e07d1905942a245a3a156299ba15369cc57
Rana0000 <memmedovarena2004@gmail.com>
2023-05-30 12:31:20 +0400

    Hotel Managment/ Html/CSS Php

--- admin/inc/header.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <div class="nav-wrapper">
-    <a href="../index.php">Back to the site</a>&nbsp;&nbsp;&nbsp; 
-    <a href="add_hotel.php">Add Hotel</a>&nbsp;&nbsp;&nbsp;
-    <a href="logout.php">Exit</a>&nbsp;&nbsp;&nbsp;
-    </div>
-</nav>
-<hr>
--- functions/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "config/config.php";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
